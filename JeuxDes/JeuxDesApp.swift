e04489758c76fe9a0ac3e362fd855fe309b2c04f
David <chalumeau.david+github@gmail.com>
2021-04-26 20:43:57 +0200

    Initial Commit

---
+++ JeuxDes/JeuxDesApp.swift
@@ -0,0 +1,17 @@
+//
+//  JeuxDesApp.swift
+//  JeuxDes
+//
+//  Created by David CHALUMEAU on 26/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct JeuxDesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
